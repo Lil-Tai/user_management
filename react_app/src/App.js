0f62c60c6c5b07474bcef1ae40ead65007036633
Tai Nguyen <dinhtai040197@gmail.com>
2020-10-25 00:09:24 +0300

    detail and select pages for events

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import EventsItem from './components/EventsItem';
+import DetailsEvents from './components/DetailsEvents';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      join: "info",
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:5000/events').then(result => {
+          this.setState({ events: result.data.events})
+          console.log(result)
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
+  getEventsInfo = (getEventsId) => {
+    return this.state.events.find(events => events.id === getEventsId);
+  }
+
+  getJoinEvent = (e) => {
+    e.preventDefault()
+    if(this.state.join === "info"){
+      this.setState({
+        join: "danger"
+      })
+    }else{
+      this.setState({
+        join: "info"
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path='/' exact render={routeProps => <EventsItem events={this.state.events} {...routeProps} />} />
+          <Route path='/events/:id' exact render={routeProps => <DetailsEvents getEventsInfo={this.getEventsInfo} join={this.state.join} getJoinEvent={this.getJoinEvent} {...routeProps} />} />
+        </div>
+      </Router>
+
+    );
+
+  }
 }
 
-export default App;
+
